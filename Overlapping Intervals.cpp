12947fdb455cb0a1ecd8e18a89470f777cf6d6b4
Krup Patel <krup3112@gmail.com>
2024-12-08 10:36:29 +0530

    Create Overlapping Intervals.cpp

---
+++ Overlapping Intervals.cpp
@@ -0,0 +1,20 @@
+vector<vector<int>> mergeOverlap(vector<vector<int>>& arr) {
+        // Code here
+        vector<vector<int>> ans;
+        
+        sort(arr.begin(),arr.end());
+        
+        int n = arr.size();
+        
+        ans.push_back(arr[0]);
+        
+        for(int i = 1;i<n;i++){
+            if(arr[i][0] <= ans.back()[1]){
+                ans.back()[1] = max(arr[i][1],ans.back()[1]);
+            }else{
+                ans.push_back(arr[i]);
+            }
+        }
+        
+        return ans;
+    }
